7df0170b94458d6afd595d6a87a72ec1ab6b6291
RSR312-206 <roccosummer@gmail.com>
2015-08-18 22:35:42 -0700

    added reset button

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 
 
-var gameOptions = document.querySelector('gameoptions');
 var scoreTally = 0;
+var lossTally = 0;
 
 var score = document.getElementById('score');
-score.innerHTML = 'your current score is ' + scoreTally;
+score.innerHTML = 'wins ' + scoreTally;
 
+var loss = document.getElementById('losses');
+loss.innerHTML = 'losses ' + lossTally;
 
 function randomAnswer() {
   var options = ['rock', 'paper', 'scissors', 'rock', 'paper', 'scissors'];
   var answer = options[Math.floor(Math.random()*options.length)];
    return answer;
- };
+ }
+
 
 var rock = document.getElementById('rock');
 rock.addEventListener("click", rockResults);
 
-
 function rockResults() {
   var computerAnswer = randomAnswer();
   if (computerAnswer == 'paper') {
+    lossTally += 1;
     alert('paper. you lost to a computer');
   } else if (computerAnswer  == 'scissors') {
     scoreTally += 1;
-    alert('scissors, you won')
+    alert('scissors, you won');
   } else if (computerAnswer  == 'rock') {
     alert('rock, a tie');
   }
-score.innerHTML = 'your current score is ' + scoreTally;
+score.innerHTML = 'wins ' + scoreTally;
+loss.innerHTML = 'losses ' + lossTally;
+
 }
 
 var paper = document.getElementById('paper');
@@ -38,12 +43,14 @@ function paperResults() {
   if (computerAnswer == 'paper') {
     alert('paper. A tie');
   } else if (computerAnswer == 'scissors') {
+    lossTally += 1;
     alert('scissors, you lost to a computer');
   } else if (computerAnswer == 'rock') {
     scoreTally += 1;
     alert('paper, you won');
   }
-score.innerHTML = 'your current score is ' + scoreTally;
+score.innerHTML = 'wins ' + scoreTally;
+loss.innerHTML = 'losses ' + lossTally;
 }
 
 
@@ -55,27 +62,21 @@ function scissorsResults() {
   if (computerAnswer == 'scissors') {
     alert('scissors. a tie');
   } else if (computerAnswer == 'paper') {
-    scoreTally += 1
+    scoreTally += 1;
     alert('paper, you win');
   } else if (computerAnswer == 'rock') {
+    lossTally += 1;
     alert('rock. you lose');
   }
-score.innerHTML = 'your current score is ' + scoreTally;
+score.innerHTML = 'wins ' + scoreTally;
+loss.innerHTML = 'losses ' + lossTally;
 }
 
 
-
-
-
-
-
-
-
-
-
-
-// gameOptions.addEventListner("click", aTurn);
-
-//   function aTurn(e) {
-//     e.target.dataset.gameOptions;
-//   }
+var button  = document.querySelector('button');
+button.addEventListener("click", function() {
+  scoreTally = 0;
+  lossTally = 0;
+  score.innerHTML = 'wins ' + scoreTally;
+  loss.innerHTML = 'losses ' + lossTally;
+  });
