20406ebe445a469e47d89dd24e617623c54d3347
RSR312-206 <roccosummer@gmail.com>
2015-08-20 16:45:36 -0700

    fixed jquery

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 var score = document.getElementById('score');
 var loss = document.getElementById('losses');
 var button  = document.querySelector('button');
-var scissors = document.getElementById('scissors');
-var paper = document.getElementById('paper');
-var rock = document.getElementById('rock');
-var divs = document.getElementById('alldivs');
-
 var scoreTally = 0;
 var lossTally = 0;
 
-$('#alldivs').on('click', function(event) {
-  console.log(event.target);
-  if (event.target == paper) {
-    return paperResults();
-  } else if (event.target == rock) {
-    return rockResults();
-  } else if (event.target == scissors) {
-   return scissorsResults();
- }
+$('#allDivs').on('click', 'div', function(event) {
+  console.log(event.target.id);
+  if (this.id === 'paper') {
+    paperResults();
+  } else if (this.id === 'rock') {
+    rockResults();
+  } else if (this.id === 'scissors') {
+    scissorsResults();
+  }
 
 });
-
 
 function randomAnswer() {
   var options = ['rock', 'paper', 'scissors', 'rock', 'paper', 'scissors'];
@@ -33,45 +27,47 @@ function tallyUpdate() {
   loss.innerHTML = 'losses ' + lossTally;
 }
 
-
 function rockResults() {
+  console.log('I made it rock!');
   var computerAnswer = randomAnswer();
   if (computerAnswer == 'paper') {
     lossTally += 1;
-    alert('paper. you lost to a computer');
+    alert('paper. you lost to a computer ');
   } else if (computerAnswer  == 'scissors') {
     scoreTally += 1;
-    alert('scissors, you won');
+    alert('scissors, you won ');
   } else if (computerAnswer  == 'rock') {
-    alert('rock, a tie');
+    alert('rock, a tie ');
   }
   tallyUpdate();
 }
 
 function paperResults() {
+  console.log('I made it paper!');
   var computerAnswer = randomAnswer();
   if (computerAnswer == 'paper') {
-    alert('paper. A tie');
+    alert('paper. A tie ');
   } else if (computerAnswer == 'scissors') {
     lossTally += 1;
-    alert('scissors, you lost to a computer');
+    alert('scissors, you lost to a computer ');
   } else if (computerAnswer == 'rock') {
     scoreTally += 1;
-    alert('paper, you won');
+    alert('paper, you won ');
   }
   tallyUpdate();
 }
 
 function scissorsResults() {
+  console.log('I made it scissors!')
   var computerAnswer = randomAnswer();
   if (computerAnswer == 'scissors') {
-    alert('scissors. a tie zzz');
+    alert('scissors. a tie ');
   } else if (computerAnswer == 'paper') {
     scoreTally += 1;
-    alert('paper, you win zzz');
+    alert('paper, you win ');
   } else if (computerAnswer == 'rock') {
     lossTally += 1;
-    alert('rock. you lose zzzz');
+    alert('rock. you lose ');
   }
   tallyUpdate();
 
@@ -84,18 +80,3 @@ button.addEventListener("click", function() {
   loss.innerHTML = 'losses ' + lossTally;
 });
 
-
-
-//divs.addEventListener("click", playerClick);
-// $('.box').on("click", function(event) {
-//     if ($(this).children().length > 0) {
-//       $(this).empty();
-//     } else $(this).html('<img width = "90%" length = "90%" src=http://cdn.cutestpaw.com/wp-content/uploads/2012/02/Dachshund-puppy-wearing-a-sweater-l.jpg>');
-//   });
-// function playerClick(e) {
-//   if (this.target == paper) {
-//     paperResults();
-//   } else if (this.target == rock) {
-//     rockResults();
-//   } else scissorsResults();
-// }
